5315642c52fe3ef5e6f5afe07353f187a6e4360e
Nima Jamalian <nima.jamalian80@gmail.com>
2024-03-31 22:51:23 +0100

    VSCODE

---
+++ Algorithms & Data Structures Problems/Leetcode/1. Two Sum [Easy, Array, Hashmap]/TwoSum.cpp
@@ -0,0 +1,72 @@
+/*
+1. Two Sum
+Topics: Array, Hash Table
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+ 
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+*/
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+// Time O(n) | Space O(n)
+vector<int> twoSum(vector<int> &nums, int target)
+{
+    unordered_map<int, int> map;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (map.contains(nums[i]))
+        {
+            return vector<int>{map[nums[i]], i};
+        }
+        else
+        {
+            int diff = target - nums[i];
+            map[diff] = i;
+        }
+    }
+    return {};
+}
+
+int main(int argc, char const *argv[])
+{
+    vector<int> test = {2,7,11,15};
+    int target = 9;
+    vector<int> result = twoSum(test,target);
+    for(auto i:result){
+        cout << i << "\n";
+    }
+    return 0;
+}
+
+
